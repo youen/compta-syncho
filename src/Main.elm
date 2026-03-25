a36cddb4483d6128bc5b4f7bd31b0368e760c3de
Youen Péron <youen.peron@gmail.com>
2026-03-25 23:48:44 +0100

    feat(Main): handle form inputs for US #1 configuration

--- src/Main.elm
+++ src/Main.elm
@@ -2,38 +2,75 @@ module Main exposing (Model(..), Msg(..), init, main, update)
 
 import Browser
 import Caisse exposing (Caisse)
-import Html exposing (Html, button, div, h1, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, button, div, h1, input, label, text)
+import Html.Attributes exposing (placeholder, type_, value)
+import Html.Events exposing (onClick, onInput)
 
 
 type Model
-    = Configuration
+    = Configuration { fondSaisi : String, jetonsSaisis : String }
     | EnService Caisse
 
 
 type Msg
-    = OuvrirCaisse Int Int
+    = SetFond String
+    | SetJetons String
+    | ValiderConfiguration
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Configuration, Cmd.none )
+    ( Configuration { fondSaisi = "", jetonsSaisis = "" }, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        OuvrirCaisse fond jetons ->
-            ( EnService (Caisse.ouvrir fond jetons), Cmd.none )
+        SetFond val ->
+            case model of
+                Configuration config ->
+                    ( Configuration { config | fondSaisi = val }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        SetJetons val ->
+            case model of
+                Configuration config ->
+                    ( Configuration { config | jetonsSaisis = val }, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        ValiderConfiguration ->
+            case model of
+                Configuration { fondSaisi, jetonsSaisis } ->
+                    case ( String.toInt fondSaisi, String.toInt jetonsSaisis ) of
+                        ( Just fond, Just jetons ) ->
+                            ( EnService (Caisse.ouvrir fond jetons), Cmd.none )
+
+                        _ ->
+                            ( model, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
     case model of
-        Configuration ->
+        Configuration { fondSaisi, jetonsSaisis } ->
             div []
                 [ h1 [] [ text "Configuration Initiale" ]
-                , button [ onClick (OuvrirCaisse 150 1000) ] [ text "Ouvrir la caisse" ]
+                , div []
+                    [ label [] [ text "Fond de caisse (€)" ]
+                    , input [ type_ "number", placeholder "Ex: 150", value fondSaisi, onInput SetFond ] []
+                    ]
+                , div []
+                    [ label [] [ text "Nombre de jetons initial" ]
+                    , input [ type_ "number", placeholder "Ex: 1000", value jetonsSaisis, onInput SetJetons ] []
+                    ]
+                , button [ onClick ValiderConfiguration ] [ text "Ouvrir la caisse" ]
                 ]
 
         EnService _ ->
--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -9,29 +9,70 @@ import Test exposing (..)
 suite : Test
 suite =
     describe "Main - Flux de l'application (US #1)"
-        [ test "L'envoi du message OuvrirCaisse passe le modèle en mode EnService avec la Caisse initialisée" <|
+        [ test "L'envoi des messages SetFond et SetJetons met à jour le modèle de Configuration" <|
+            \_ ->
+                let
+                    ( model1, _ ) =
+                        init ()
+
+                    ( model2, _ ) =
+                        update (SetFond "150") model1
+
+                    ( model3, _ ) =
+                        update (SetJetons "1000") model2
+                in
+                case model3 of
+                    Configuration { fondSaisi, jetonsSaisis } ->
+                        Expect.all
+                            [ \_ -> Expect.equal "150" fondSaisi
+                            , \_ -> Expect.equal "1000" jetonsSaisis
+                            ]
+                            ()
+
+                    EnService _ ->
+                        Expect.fail "Le modèle ne devrait pas être EnService"
+        , test "L'envoi du message ValiderConfiguration avec des entrées valides passe le modèle en mode EnService avec la Caisse initialisée" <|
             \_ ->
                 let
                     ( initialModel, _ ) =
                         init ()
 
-                    fondEnEuros =
-                        150
+                    -- On configure les entrées
+                    ( modelConf1, _ ) =
+                        update (SetFond "150") initialModel
 
-                    jetonsInitiaux =
-                        1000
+                    ( modelConf2, _ ) =
+                        update (SetJetons "1000") modelConf1
 
                     ( updatedModel, _ ) =
-                        update (OuvrirCaisse fondEnEuros jetonsInitiaux) initialModel
+                        update ValiderConfiguration modelConf2
                 in
                 case updatedModel of
                     EnService caisse ->
                         Expect.all
-                            [ \c -> Expect.equal jetonsInitiaux (Caisse.stockTotal c)
-                            , \c -> Expect.equal fondEnEuros (Caisse.fondDeCaisse c)
+                            [ \c -> Expect.equal 1000 (Caisse.stockTotal c)
+                            , \c -> Expect.equal 150 (Caisse.fondDeCaisse c)
                             ]
                             caisse
 
-                    Configuration ->
+                    Configuration _ ->
                         Expect.fail "Le modèle est resté bloqué sur l'écran de configuration."
+        , test "L'envoi du message ValiderConfiguration avec des entrées invalides ne passe pas en mode EnService" <|
+            \_ ->
+                let
+                    ( initialModel, _ ) =
+                        init ()
+
+                    ( modelConf, _ ) =
+                        update (SetFond "abc") initialModel
+
+                    ( updatedModel, _ ) =
+                        update ValiderConfiguration modelConf
+                in
+                case updatedModel of
+                    Configuration _ ->
+                        Expect.pass
+
+                    EnService _ ->
+                        Expect.fail "Le modèle ne devrait pas passer EnService avec des entrées invalides."
         ]
