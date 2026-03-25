cf3f388ec9f489612a4eaef6a1dc3f8d0fb033fe
Youen Péron <youen.peron@gmail.com>
2026-03-25 23:46:42 +0100

    feat(Caisse): initialization logic for stock and fond (US #1)

---
+++ src/Caisse.elm
@@ -0,0 +1,26 @@
+module Caisse exposing (Caisse, ouvrir, stockTotal, stockCaisse, fondDeCaisse)
+
+type Caisse
+    = Caisse
+        { fondEnEuros : Int
+        , jetonsInitiaux : Int
+        }
+
+ouvrir : Int -> Int -> Caisse
+ouvrir fond jetons =
+    Caisse
+        { fondEnEuros = fond
+        , jetonsInitiaux = jetons
+        }
+
+stockTotal : Caisse -> Int
+stockTotal (Caisse c) =
+    c.jetonsInitiaux
+
+stockCaisse : Caisse -> Int
+stockCaisse (Caisse c) =
+    c.jetonsInitiaux
+
+fondDeCaisse : Caisse -> Int
+fondDeCaisse (Caisse c) =
+    c.fondEnEuros
---
+++ tests/CaisseTest.elm
@@ -0,0 +1,24 @@
+module CaisseTest exposing (..)
+
+import Expect
+import Test exposing (..)
+import Caisse exposing (Caisse)
+
+suite : Test
+suite =
+    describe "Caisse - Initialisation (US #1)"
+        [ test "Ouvrir la caisse doit assigner le stock total au stock caisse, et conserver le fond de caisse" <|
+            \_ ->
+                let
+                    fondEnEuros = 150
+                    jetonsInitiaux = 1000
+                    caisse : Caisse
+                    caisse = Caisse.ouvrir fondEnEuros jetonsInitiaux
+                in
+                Expect.all
+                    [ \c -> Expect.equal jetonsInitiaux (Caisse.stockTotal c)
+                    , \c -> Expect.equal jetonsInitiaux (Caisse.stockCaisse c)
+                    , \c -> Expect.equal fondEnEuros (Caisse.fondDeCaisse c)
+                    ]
+                    caisse
+        ]
